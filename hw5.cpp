1551f3c4ead216ef029f5ede6dc5b89aff01c3c2
stephenburling <stephenburling99@gmail.com>
2020-10-05 22:50:34 -0500

    finishing up styling, jsut saving

--- hw5.cpp
+++ hw5.cpp
@@ -1,97 +1,96 @@
+/*
+BubbleSort:
+  initiate bool x to true
+  if size of passed vector is 0, set x to false
+
+*/
 #include <iostream>
 #include <vector>
 #include <string>
 
-void BubbleSort(std::vector<std::string>& vec) {
+void BubbleSort(std::vector<std::string>& v) {
   bool x = true;
-  if (vec.size() == 0) {
+  if (v.size() == 0) {
       x = false;
   }
   while (x == true) {
     x = false;
-    for (int i = 0; i < (vec.size()-1); i++) {
-      if (vec[i].compare(vec[i + 1]) > 0) {
-      swap(vec[i], vec[i+1]);
+    for (int i = 0; i < (v.size()-1); i++) {
+      if (v[i].compare(v[i + 1]) > 0) {
+      swap(v[i], v[i+1]);
       x = true;
       }
     }
   }
 }
 
-static void insertionSortRecursiveHelper(std::vector<int>& vec, int size)
-{
+static void insertionSortRecursiveHelper(std::vector<int>& v, int size) {
     if (size <= 1) {
         return;
     }
 
-    insertionSortRecursiveHelper(vec, size - 1);
+    insertionSortRecursiveHelper(v, size - 1);
 
-    int last_element = vec[size - 1];
+    int last_element = v[size - 1];
     int index = size - 2;
 
-    while (index >= 0 && vec[index] > last_element)
-    {
-        vec[index + 1] = vec[index];
+    while (index >= 0 && v[index] > last_element){
+        v[index + 1] = v[index];
         index--;
     }
 
-    vec[index + 1] = last_element;
+    v[index + 1] = last_element;
 }
 
 void InsertionSort(std::vector<int>& v) {
-
     insertionSortRecursiveHelper(v, v.size());
-
 }
 
-void MergeSort(std::vector<std::string>& vec) {
+void MergeSort(std::vector<std::string>& v) {
+  if (v.size() == 1) {
+    return;
+  }
+  int size = v.size();
+  int middle = size / 2;
 
-    if (vec.size() == 1) {
-        return;
+  std::vector<std::string> left_vec(v.begin(),v.begin()+middle);
+  std::vector<std::string> right_vec(v.begin() + middle, v.end());
+
+  if (v.size() > 2) {
+    MergeSort(left_vec);
+    MergeSort(right_vec);
+  }
+  int left_counter = 0;
+  int right_counter = 0;
+  int vec_counter = 0;
+
+  while (left_counter < left_vec.size() && right_counter < right_vec.size()) {
+    if (left_vec[left_counter].compare(right_vec[right_counter]) < 0) {
+      v[vec_counter] = left_vec[left_counter];
+      left_counter++;
+
+    }else if (left_vec[left_counter].compare(right_vec[right_counter]) == 0) {
+      v[vec_counter] = left_vec[left_counter];
+      left_counter++;
+      vec_counter++;
+
+      v[vec_counter] = right_vec[right_counter];
+      right_counter++;
     }
-        int last = vec.size();
-        int middle = last / 2;
-
-        auto left = vec.begin();
-        auto midpoint = vec.begin() + middle;
-        auto right = vec.end();
-
-        std::vector<std::string> leftVec(left,midpoint);
-        std::vector<std::string> rightVec(midpoint, right);
-
-    if (vec.size() > 2) {
-        MergeSort(leftVec);
-        MergeSort(rightVec);
+    else {
+      v[vec_counter] = right_vec[right_counter];
+      right_counter++;
     }
-        int leftCounter = 0;
-        int rightCounter = 0;
-        int vecCounter = 0;
-
-        while (leftCounter < leftVec.size() && rightCounter < rightVec.size()) {
-            if (leftVec[leftCounter].compare(rightVec[rightCounter]) < 0) {
-                vec[vecCounter] = leftVec[leftCounter];
-                  leftCounter++;
-            }else if (leftVec[leftCounter].compare(rightVec[rightCounter]) == 0) {
-                vec[vecCounter] = leftVec[leftCounter];
-                  leftCounter++;
-                  vecCounter++;
-                vec[vecCounter] = rightVec[rightCounter];
-                  rightCounter++;
-            }
-            else {
-                vec[vecCounter] = rightVec[rightCounter];
-                  rightCounter++;
-            }
-                  vecCounter++;
-        }
-        while (leftCounter < leftVec.size()) {
-            vec[vecCounter] = leftVec[leftCounter];
-              leftCounter++;
-              vecCounter++;
-        }
-        while (rightCounter < rightVec.size()) {
-            vec[vecCounter] = rightVec[rightCounter];
-              rightCounter++;
-              vecCounter++;
-        }
+    vec_counter++;
+  }
+  while (left_counter < left_vec.size()) {
+    v[vec_counter] = left_vec[left_counter];
+    left_counter++;
+    vec_counter++;
+  }
+  while (right_counter < right_vec.size()) {
+    v[vec_counter] = right_vec[right_counter];
+    right_counter++;
+    vec_counter++;
+  }
 }
--- main.cpp
+++ main.cpp
@@ -23,8 +23,18 @@ int main() {
     vi = {4,1,7,5,6,9};
     v = { "emma", "dora", "ruth", "stephen", "alisha", "alex", "ziegellaub", "eichler" };
    
-    MergeSort(v);
-    std::cout << "Testing vector: " << std::endl;
-    print(v);
+    //MergeSort(v);
+   // std::cout << "Testing vector: " << std::endl;
+    
+    InsertionSort(vi);
+
+    for (int i = 0; i < vi.size(); i++) {
+        std::cout << vi[i] << std::endl;
+    }
+
+    auto begin = vi.begin();
+    auto end = vi.end();
+    std::vector<int> tempV(begin, end-4);
+
 
 }
