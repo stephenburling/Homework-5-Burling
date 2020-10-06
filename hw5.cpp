da69cc355473bcd984c4dca17e265d0784650db2
stephenburling <stephenburling99@gmail.com>
2020-10-05 23:21:48 -0500

    final changes

--- hw5.cpp
+++ hw5.cpp
@@ -1,8 +1,52 @@
 /*
 BubbleSort:
-  initiate bool x to true
-  if size of passed vector is 0, set x to false
+    initiate bool x to true
+    if size of passed vector is 0, set x to false
+    while x is true
+        set x to false
+        for each element in the vector
+            if the current element is greater than the next element (or further down the alphabet)
+                swap the current element and the next element
+                set x to true.
 
+InsertionSortRecursiveHelper
+    if size of passed vector is 0, return 
+    recursively call InsertionSortRecursiveHelper with current vector and size of vector minus one
+    initialize last_element equal to the last element found in the vector
+    initialize index to the element before last_element
+    while the index is greater than or equal to zero and the element found in the vector at the given index is greater than the last_element
+        set the next element indexed in the vector equal to the current indexed element
+        decrement the index
+    set the next element indexed in vector equal to last_element
+
+InsertionSort
+    call InsertionSortRecursiveHelper with passed vector and size of vector
+
+MergeSort
+    if size of passed vector is equal to 0, return 
+    intialize int middle to equal the size of the vector divided by two
+    initialize subvector left_vec with the contents of passed vector v from beginning of vector to the midpoint
+    initialize subvector right_vec with the contents of passed vector v from midpoint of vector to the end
+    if current v size is greater than two
+        recursively call MergeSort with left_vec and again with right_vec
+    initialize left_counter, right_counter, and vec_counter to zero
+    while left_counter is less than the size of left_vec AND right_counter is less than the size of right_vec
+        if current element indexed in left_vec is less than the current element indexed in right_vec
+            set current element in main vector to the element in left_vec
+        else, if current elements in both left_vec and right_vec are equal
+            set current element in main vector to the element in left_vec
+            increment left_vec
+            increment main vector 
+            set current element in main vector to the element in right vec
+            increment right_vec
+        else,
+            set current element in main vector to the element in right vec
+            increment right_vec
+        increment main vector
+    while there are still elements in left_vec
+        copy elements into main vector
+    while there are still elements in right_vec
+        copy elements into main vector
 */
 #include <iostream>
 #include <vector>
@@ -24,12 +68,12 @@ void BubbleSort(std::vector<std::string>& v) {
   }
 }
 
-static void insertionSortRecursiveHelper(std::vector<int>& v, int size) {
+static void InsertionSortRecursiveHelper(std::vector<int>& v, int size) {
     if (size <= 1) {
         return;
     }
 
-    insertionSortRecursiveHelper(v, size - 1);
+    InsertionSortRecursiveHelper(v, size - 1);
 
     int last_element = v[size - 1];
     int index = size - 2;
@@ -43,15 +87,15 @@ static void insertionSortRecursiveHelper(std::vector<int>& v, int size) {
 }
 
 void InsertionSort(std::vector<int>& v) {
-    insertionSortRecursiveHelper(v, v.size());
+    InsertionSortRecursiveHelper(v, v.size());
 }
 
 void MergeSort(std::vector<std::string>& v) {
-  if (v.size() == 1) {
-    return;
-  }
-  int size = v.size();
-  int middle = size / 2;
+    if (v.size() == 1) {
+        return;
+    }
+
+  int middle = v.size() / 2;
 
   std::vector<std::string> left_vec(v.begin(),v.begin()+middle);
   std::vector<std::string> right_vec(v.begin() + middle, v.end());
--- main.cpp
+++ main.cpp
@@ -17,24 +17,13 @@ void print(std::vector<std::string>& v) {
 }
 
 int main() {
-
     std::vector<std::string> v;
     std::vector<int> vi;
+
     vi = {4,1,7,5,6,9};
     v = { "emma", "dora", "ruth", "stephen", "alisha", "alex", "ziegellaub", "eichler" };
-   
-    //MergeSort(v);
-   // std::cout << "Testing vector: " << std::endl;
-    
+
+    BubbleSort(v);
     InsertionSort(vi);
-
-    for (int i = 0; i < vi.size(); i++) {
-        std::cout << vi[i] << std::endl;
-    }
-
-    auto begin = vi.begin();
-    auto end = vi.end();
-    std::vector<int> tempV(begin, end-4);
-
-
+    MergeSort(v);
 }
